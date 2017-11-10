d4079817ac9eebc5041b73a6ff9b62bae8b1c57b
Kyle Begovich <begovichkyle@gmail.com>
2017-11-10 02:40:03 -0600

    forex updates

--- darkpools/forex.py
+++ darkpools/forex.py
@@ -1,15 +1,17 @@
 import tradersbot
-import monotonic as clock
+import time
 
 t = tradersbot.TradersBot('127.0.0.1', 'trader0', 'trader0')
 tickers = ['USDCAD', 'EURUSD', 'USDCHF', 'USDJPY', 'EURCAD', 'EURJPY', 'EURCHF', 'CHFJPY']
-unseen_tickers = set(['USDCAD', 'EURUSD', 'USDCHF', 'USDJPY'])
 print("starting")
 
 # the book as we know it
 orderbook = dict(USDCAD={"bids": {}, "asks": {}}, USDJPY={"bids": {}, "asks": {}}, EURUSD={"bids": {}, "asks": {}},
                  USDCHF={"bids": {}, "asks": {}}, CHFJPY={"bids": {}, "asks": {}}, EURJPY={"bids": {}, "asks": {}},
                  EURCHF={"bids": {}, "asks": {}}, EURCAD={"bids": {}, "asks": {}})
+
+# spring constants for the bias towards USD
+springs = dict(CAD=1, JPY=1, EUR=1, CHF=1)
 
 # everything we need to know about our state of being
 traderstate = {'cash': {"USD": 100000}, 'positions': {}, 'open_orders': {}, 'pnl': {"USD": 0}, 'time': '1',
@@ -22,8 +24,6 @@ darktickers = ['EURCHF', 'EURCAD', 'EURJPY', 'CHFJPY']
 edges = {'USDCAD': 0, 'EURUSD': 0, 'USDCHF': 0, 'USDJPY': 0, 'EURCAD': 0, 'EURJPY': 0, 'EURCHF': 0, 'CHFJPY': 0,
          'CADUSD': 0, 'USDEUR': 0, 'CHFUSD': 0, 'JPYUSD': 0, 'CADEUR': 0, 'JPYEUR': 0, 'CHFEUR': 0, 'JPYCHF': 0}
 
-
-
 # by dark pool order
 triangles = {'EURCHF': ('USDCHF', 'EURUSD'), 'EURCAD': ('USDCAD', 'EURUSD'),
              'EURJPY': ('USDJPY', 'EURUSD'), 'CHFJPY': ('USDJPY', 'USDCHF')}
@@ -31,12 +31,10 @@ triangles = {'EURCHF': ('USDCHF', 'EURUSD'), 'EURCAD': ('USDCAD', 'EURUSD'),
 openorders = {}
 
 
-def marketUpdate(msg, order):
-    #print("market update")
+def market_update(msg, order):
     global orderbook
     global openorders
-    #print(msg)
-    spread = 1.5
+
     index = msg["market_state"]["ticker"]
     bids = msg["market_state"]["bids"]
     asks = msg["market_state"]["asks"]
@@ -46,7 +44,6 @@ def marketUpdate(msg, order):
 
     startedge = index[:3]
     endedge = index[3:]
-    # the stuff above: will doing this on every update take too much time?
 
     if bids != {}:
         listbids = list(msg["market_state"]["bids"].keys())
@@ -59,88 +56,64 @@ def marketUpdate(msg, order):
         edges[endedge + startedge] = lowestask
 
     for darksecurity in darktickers:
-        updatedarkedges(darksecurity, order)  # update the arbitrage market prices
+        update_dark_edges(darksecurity, order)  # update the arbitrage market prices
 
 
     if index == "USDCHF":
-        opendarkorder('EURCHF', order, .11)
+        open_dark_order('EURCHF', order, .11)
 
     elif index == "EURUSD":
-        opendarkorder('EURJPY', order, 1.5)
+        open_dark_order('EURJPY', order, 1.5)
 
     elif index == 'USDJPY':
-        opendarkorder('CHFJPY', order, 1.5)
+        open_dark_order('CHFJPY', order, 1.5)
 
     elif index == 'USDCAD':
-        opendarkorder('EURCAD', order, .11)
+        open_dark_order('EURCAD', order, .11)
 
     # delete open orders after 2.5 seconds...too many orders
     deletedorders = []
     for everyorder in openorders:
-        print 'l1'
         if openorders[everyorder][0]['ticker'] in darktickers:
-            print 'l2'
-            if (clock.monotonic() - openorders[everyorder][1]) > 2.0:
-                print 'l3'
+            if (time.time() - openorders[everyorder][1]) > 2.0:
                 order.addCancel(openorders[everyorder][0]['ticker'], openorders[everyorder][0]['order_id'])
                 deletedorders.append(everyorder)
         else:
-            print 'l4'
-            if (clock.monotonic() - openorders[everyorder][1]) > 4:
-                print 'l5'
-                print 'openorders[everyorder] =', openorders[everyorder][0]
+            if (time.time() - openorders[everyorder][1]) > 4:
                 order.addCancel(openorders[everyorder][0]['ticker'], openorders[everyorder][0]['order_id'])
                 deletedorders.append(everyorder)
 
     for i in deletedorders:
         del openorders[i]
 
-    #print(traderstate)
-    #print(edges)
-    #print(orderbook)
-    
-    do_trades(0.01)
+    update_springs()
 
 
-def updatedarkedges(darksecurity, order):
+def update_springs():
+    print("update springs")
+
+
+
+def update_dark_edges(darksecurity, order):
     if darksecurity == 'CHFJPY':
-        if edges[triangles[darksecurity][1]] != 0:
+        if not edges[triangles[darksecurity][1]] == 0:
             edges[darksecurity] = edges[triangles[darksecurity][0]] * (1 / (edges[triangles[darksecurity][1]]))
         else:
-            print("saved error")
+            print("saved divide by zero")
+
     else:
         edges[darksecurity] = edges[triangles[darksecurity][0]] * edges[triangles[darksecurity][1]]
 
 
-def do_trades(edge_req):
-    for elem in triangles:
-        if calculate_triangle(elem) > edge_req:
-            print("make trade")
-
-
-def calculate_triangle(curr_edge):
-    dark = edges[curr_edge]
-    light1 = edges[triangles[curr_edge][0]]
-    light2 = edges[triangles[curr_edge][1]]
-    fee = 0.0001
-
-    return (dark - light1 * light2) - (3 * fee)
-
-
-def opendarkorder(darksecurity, order, spread):
+def open_dark_order(darksecurity, order, spread):
     """orders a dark currency at a price suitable for arbitrage"""
-    """if darksecurity == 'CHFJPY':
-        edges[darksecurity] = edges[triangles[darksecurity][0]] * (1/(edges[triangles[darksecurity][1]]))
 
-    else:
-        edges[darksecurity] = edges[triangles[darksecurity][0]] * edges[triangles[darksecurity][1]]"""
-
-    if len(openorders) < 24:
-        order.addTrade(darksecurity, False, 200, price=edges[darksecurity] + spread)  # 600 quantity best so far
-        order.addTrade(darksecurity, True, 200, price=edges[darksecurity] - spread)
+    if len(openorders) < 30:
+        order.addTrade(darksecurity, False, 200, edges[darksecurity] + spread)
+        order.addTrade(darksecurity, True, 200, edges[darksecurity] - spread)
 
 
-def respondDarkCompletionbuy(darksecurity, isBuy, quantity, order):
+def respond_dark_completion_buy(darksecurity, is_buy, quantity, order):
     "Responds to a dark currency being bought by selling the other two sides of the triangle"
 
     startedge1 = triangles[darksecurity][0][:3]
@@ -148,73 +121,23 @@ def respondDarkCompletionbuy(darksecurity, isBuy, quantity, order):
     startedge2 = triangles[darksecurity][1][:3]
     endedge2 = triangles[darksecurity][1][3:]
 
-    # CHFJPY special case
     if darksecurity == 'CHFJPY':
         order.addTrade("USDCHF", True, quantity, edges['USDCHF'])
         order.addTrade("USDJPY", False, quantity, edges['JPYUSD'])
     else:
-        order.addTrade(triangles[darksecurity][0], isBuy, quantity, edges[endedge1 + startedge1])
-        order.addTrade(triangles[darksecurity][1], isBuy, quantity, edges[endedge2 + startedge2])
+        order.addTrade(triangles[darksecurity][0], is_buy, quantity, edges[endedge1 + startedge1])
+        order.addTrade(triangles[darksecurity][1], is_buy, quantity, edges[endedge2 + startedge2])
 
 
-def respondDarkCompletionsell(darksecurity, isBuy, quantity, order):
+def respond_dark_completion_sell(darksecurity, is_buy, quantity, order):
     "Responds to a dark currency being sold by buying the other two sides of the triangle"
 
-    # CHFJPY special Case
     if darksecurity == 'CHFJPY':
         order.addTrade("USDCHF", False, quantity, edges['CHFUSD'])
         order.addTrade("USDJPY", True, quantity, edges['USDJPY'])
     else:
-        order.addTrade(triangles[darksecurity][0], isBuy, quantity,
-                       edges[triangles[darksecurity][0]])  # TODO which price to use, current market?
-        order.addTrade(triangles[darksecurity][1], isBuy, quantity, edges[triangles[darksecurity][1]])
-
-
-def periodicTraderUpdate(msg, order):
-    "update the traderstate"
-
-    global traderstate
-
-    for item in msg["trader_state"]:
-        traderstate[item] = msg["trader_state"][item]
-
-    # print(traderstate)
-
-    # avoiding hitting position limits
-    """if traderstate['pnl']['EUR'] < 90000:
-        reachingpositionlimitnegative('EUR', order)
-    elif traderstate['pnl']['EUR'] > 90000:
-        reachingpositionlimitpositive('EUR', order)
-
-    if traderstate['pnl']['CAD'] < 90000:
-        reachingpositionlimitnegative('CAD', order)
-    elif traderstate['pnl']['EUR'] > 90000:
-        reachingpositionlimitpositive('CAD', order)
-
-    if traderstate['pnl']['EUR'] < 90000:
-        reachingpositionlimitnegative('CHF', order)
-    elif traderstate['pnl']['EUR'] > 90000:
-        reachingpositionlimitpositive('CHF', order)
-
-    if traderstate['pnl']['JPY'] < 9000000:
-        reachingpositionlimitnegative('JPY', order)
-    elif traderstate['pnl']['EUR'] > 9000000:
-        reachingpositionlimitpositive('JPY', order)"""
-
-
-def reachingpositionlimitpositive(currency, order):
-    """converts currency back to USD to prevent reaching position limits"""
-    if currency != 'EUR':
-        order.addTrade("USD" + 'currency', True, 50, edges[currency + "USD"])
-    else:
-        order.addTrade('EURUSD', False, 50, edges["USDEUR"])
-
-
-def reachingpositionlimitnegative(currency, order):
-    if currency != 'EUR':
-        order.addTrade('USD' + currency, False, 50, edges[currency + 'USD'])
-    else:
-        order.addTrade('EURUSD', True, 50, edges["EURUSD"])
+        order.addTrade(triangles[darksecurity][0], is_buy, quantity, edges[triangles[darksecurity][0]])
+        order.addTrade(triangles[darksecurity][1], is_buy, quantity, edges[triangles[darksecurity][1]])
 
 
 def reactOnTrade(msg, order):
@@ -236,11 +159,11 @@ def reactOnTrade(msg, order):
         # trading the triangle
         if ticker in darktickers:
             if buyid in openorders:
-                respondDarkCompletionbuy(ticker, False, quantity, order)  # sell the other two sides of the triangle
+                respond_dark_completion_buy(ticker, False, quantity, order)  # sell the other two sides of the triangle
                 del openorders[buyid]
 
             elif sellid in openorders:
-                respondDarkCompletionsell(ticker, True, quantity, order)  # buy the other two sides of the triangle
+                respond_dark_completion_sell(ticker, True, quantity, order)  # buy the other two sides of the triangle
                 del openorders[sellid]
 
         # if other orders completed, take them out of the openorders
@@ -268,35 +191,23 @@ def reactOnTrade(msg, order):
 
 def acknowledgedOrders(msg, order):
     global openorders
-    #print(msg)
+
     if 'orders' in msg:
         orders = msg["orders"]
 
-        for eachorder in orders:  # process each order, add to openorders
-            #print(eachorder)
-            openorders[eachorder['order_id']] = eachorder, clock.monotonic()
-            #print(openorders)
+        for elem in orders:  # process each order, add to openorders
+            openorders[elem['order_id']] = elem, time.time()
 
 
-last_reset = 0.0
-def onTraderUpdate(msg, order):
-    global last_reset
-    ts = clock.monotonic()
-    try:
-        if ts - last_rest > 5:
-            last_reset = ts
-            for open_order in msg['trader_state']['open_orders']:
-                order.addCancel(open_order['ticker'], open_order['order_id'])
-    except KeyError:
-        print 'failed cancelAll loop, msg =', msg
+def verify_trader_state(msg, order):
+    print(msg)
+    global traderstate
+
+    for elem in msg["trader_state"]:
+        traderstate[elem] = msg["trader_state"][elem]
 
 
-
-
-
-t.onMarketUpdate = marketUpdate
-
-t.onTrade = reactOnTrade
-t.onAckModifyOrders = acknowledgedOrders
-
+t.onMarketUpdate = market_update
+t.onTraderUpdate = verify_trader_state
 t.run()
+
